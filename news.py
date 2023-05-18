c409ceef305884eedc607870f345927204e1b1dd
Simon Magus <simonmagusgno@gmail.com>
2023-05-18 16:43:08 +0200

    loop articles and get headlines

--- news.py
+++ news.py
@@ -6,11 +6,26 @@ load_dotenv()
 
 api = os.getenv('apiKey')
 
-url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api}"
+category = 'business'
+country = 'us'
+url = f"https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api}"
 
 response = requests.get(url)
 content = response.json()
 
-print(content)
+# Assuming the JSON response is stored in the 'content' variable
+articles = content['articles']  # Access the 'articles' list
+
+headlines = []  # Initialize an empty list to store the headlines
+
+# Iterate over each article and extract the 'title' key
+for article in articles:
+    title = article['title']  # Extract the 'title' key
+    headlines.append(title)  # Add the title to the headlines list
+
+# Print the extracted headlines
+for headline in headlines:
+    print(headline)
+
 
 
