207de8f90457283c20cc43813efd5ebb7e74b598
Simon Magus <simonmagusgno@gmail.com>
2023-05-18 16:34:55 +0200

    setup correctly and inital test working

--- news.py
+++ news.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={os.getenv(apiKey)}"
+api = os.getenv('apiKey')
+
+url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api}"
 
 response = requests.get(url)
 content = response.json()
